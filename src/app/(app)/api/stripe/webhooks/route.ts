90a94fb5af6256dc451dcd52e642b7e30f130f49
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-07-22 13:05:59 +0530

    feat(stripe): implement connect onboarding & custom accounts

--- src/app/(app)/api/stripe/webhooks/route.ts
+++ src/app/(app)/api/stripe/webhooks/route.ts
@@ -17,7 +17,8 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET as string,
     );
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
 
     if (error! instanceof Error) {
       console.log(error);
@@ -26,14 +27,15 @@ export async function POST(req: Request) {
     console.log(`❌ Error message: ${errorMessage}`);
     return NextResponse.json(
       { message: `Webhook Error: ${errorMessage}` },
-      { status: 400 }
-    )
+      { status: 400 },
+    );
   }
 
   console.log("✅ Success:", event.id);
 
   const permittedEvents: string[] = [
     "checkout.session.completed",
+    "account.updated",
   ];
 
   const payload = await getPayload({ config });
@@ -61,9 +63,8 @@ export async function POST(req: Request) {
 
           const expandedSession = await stripe.checkout.sessions.retrieve(
             data.id,
-            {
-              expand: ["line_items.data.price.product"],
-            },
+            { expand: ["line_items.data.price.product"] },
+            { stripeAccount: event.account },
           );
 
           if (
@@ -73,13 +74,15 @@ export async function POST(req: Request) {
             throw new Error("No line items found");
           }
 
-          const lineItems = expandedSession.line_items.data as ExpandedLineItem[];
+          const lineItems = expandedSession.line_items
+            .data as ExpandedLineItem[];
 
           for (const item of lineItems) {
             await payload.create({
               collection: "orders",
               data: {
                 stripeCheckoutSessionId: data.id,
+                stripeAccountId: event.account,
                 user: user.id,
                 product: item.price.product.metadata.id,
                 name: item.price.product.name,
@@ -87,11 +90,27 @@ export async function POST(req: Request) {
             });
           }
           break;
+        case "account.updated":
+          data = event.data.object as Stripe.Account;
+
+          await payload.update({
+            collection: "tenants",
+            where: {
+              stripeAccountId: {
+                equals: data.id,
+              },
+            },
+            data: {
+              stripeDetailsSubmitted: data.details_submitted,
+            },
+          });
+
+          break;
         default:
           throw new Error(`Unhandled event: ${event.type}`);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return NextResponse.json(
         { message: "Webhook handler failed" },
         { status: 500 },
@@ -100,4 +119,4 @@ export async function POST(req: Request) {
   }
 
   return NextResponse.json({ message: "Received" }, { status: 200 });
-};
+}
